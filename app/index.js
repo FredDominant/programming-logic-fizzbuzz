66c3f13058886f7c44069c6bbd39a2a9e4a7b1e1
Noblemajesty <fredadewole@gmail.com>
2017-04-08 12:38:50 +0100

    Added modified index.js file to indent properly

--- app/index.js
+++ app/index.js
@@ -1,16 +1,15 @@
 'use strict'
 
 module.exports = {
-
-fizzBuzz: function (num) {
-	if (num%15 == 0) {
-		return 'FizzBuzz';
-	} else if (num % 3 == 0){
-			return 'Fizz';
-	} else if (num%5 == 0) {
-			return 'Buzz';
-	} else {
-		return num;
+		fizzBuzz: function (num) {
+		if (num%15 == 0) {
+			return 'FizzBuzz';
+		} else if (num % 3 == 0){
+				return 'Fizz';
+		} else if (num%5 == 0) {
+				return 'Buzz';
+		} else {
+			return num;
+		}	
 	}
-}
 }
